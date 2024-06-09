826f16e1ce19f4b3848c2e055056c0b309e187fd
Omar Cruz <omar.cruzr97@hotmail.com>
2024-06-09 17:13:23 -0600

    updating

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Style from '../css/navbar.module.css'
 
 const navbar = () => {
     return (
         <nav className={Style.navbar}>
             <img src="./bocha.webp" alt="BO·CHA" className={Style.imgLogo} />
+            <div className={Style.navLinks}>
+                <Link to="/" className={Style.navLink}>Inicio</Link>
+                <Link to="/menu" className={Style.navLink}>Menú</Link>
+                <Link to="/ubicacion" className={Style.navLink}>Ubicación</Link>
+            </div>
         </nav>
     )
 }
