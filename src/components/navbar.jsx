418d06102fc9bb9338187761d334d04356178cd7
Damanger <omar.cruzr97@hotmail.com>
2024-06-10 02:17:35 -0600

    updating

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,16 +31,16 @@ const navbar = () => {
 
     return (
         <nav className={Style.navbar}>
-            <Link to="/" activeclassname="activo">
+            <Link to="/" activeclassname="activo" aria-label="Logo">
                 <img src="./bocha.webp" alt="BO·CHA" className={Style.imgLogo} onClick={closeMenu}/>
             </Link>
             <div className={Style.hamburgerMenu} onClick={toggleMenu}>
                     <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
                 </div>
             <div className={`${Style.navLinks} ${isMenuOpen ? Style.showMenu : ''}`}>
-                <Link to="/" activeclassname="activo" className={Style.navLink} style={location.pathname === "/" ? activeStyle : {}} onClick={closeMenu}><FontAwesomeIcon icon={faHome} className={Style.homeIcon} /></Link>
-                <Link to="/menu" activeclassname="activo" className={Style.navLink} style={location.pathname === "/menu" ? activeStyle : {}} onClick={closeMenu}>Menú</Link>
-                <Link to="/ubicacion" activeclassname="activo" className={Style.navLink} style={location.pathname === "/ubicacion" ? activeStyle : {}} onClick={closeMenu}>Ubicación</Link>
+                <Link to="/" activeclassname="activo" aria-label="Inicio" className={Style.navLink} style={location.pathname === "/" ? activeStyle : {}} onClick={closeMenu}><FontAwesomeIcon icon={faHome} className={Style.homeIcon} /></Link>
+                <Link to="/menu" activeclassname="activo" aria-label="Menú" className={Style.navLink} style={location.pathname === "/menu" ? activeStyle : {}} onClick={closeMenu}>Menú</Link>
+                <Link to="/ubicacion" activeclassname="activo" aria-label="Ubicación" className={Style.navLink} style={location.pathname === "/ubicacion" ? activeStyle : {}} onClick={closeMenu}>Ubicación</Link>
             </div>
         </nav>
     )
