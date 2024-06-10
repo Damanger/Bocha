5048bd369013d7f8b38f83cd82d230de9eecdab3
Damanger <omar.cruzr97@hotmail.com>
2024-06-10 00:22:32 -0600

    updating

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,9 +3,9 @@ import Style from '../css/menu.module.css';
 
 const menu = () => {
     return (
-        <div>
-            Menú
-        </div>
+        <section className={Style.fondoMenu} data-src="https://images.pexels.com/photos/4041246/pexels-photo-4041246.jpeg?auto=compress&cs=tinysrgb&w=6305&h=4203&dpr=2">
+            
+        </section>
     )
 }
 
