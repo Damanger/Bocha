8edb5ed8308f9b4896509f0aff367a524d028bbb
Omar Cruz <omar.cruzr97@hotmail.com>
2024-06-09 20:29:44 -0600

    updating

--- src/components/error404.jsx
+++ src/components/error404.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Style from '../css/error404.module.css';
 
 const error404 = () => {
     return (
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Style from '../css/menu.module.css';
 
 const menu = () => {
     return (
