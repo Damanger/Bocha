c9c2791be2cbe2bf62bf84b12e75310a669aa1fd
Damanger <omar.cruzr97@hotmail.com>
2024-06-10 02:07:36 -0600

    updating

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes, faHome, faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
 import Style from '../css/navbar.module.css'
 
 const navbar = () => {
@@ -23,13 +25,20 @@ const navbar = () => {
         setIsMenuOpen(false);
     };
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className={Style.navbar}>
             <Link to="/" activeclassname="activo">
-                <img src="./bocha.webp" alt="BO·CHA" className={Style.imgLogo} />
+                <img src="./bocha.webp" alt="BO·CHA" className={Style.imgLogo} onClick={closeMenu}/>
             </Link>
-            <div className={Style.navLinks}>
-                <Link to="/" activeclassname="activo" className={Style.navLink} style={location.pathname === "/" ? activeStyle : {}} onClick={closeMenu}>Inicio</Link>
+            <div className={Style.hamburgerMenu} onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+                </div>
+            <div className={`${Style.navLinks} ${isMenuOpen ? Style.showMenu : ''}`}>
+                <Link to="/" activeclassname="activo" className={Style.navLink} style={location.pathname === "/" ? activeStyle : {}} onClick={closeMenu}><FontAwesomeIcon icon={faHome} className={Style.homeIcon} /></Link>
                 <Link to="/menu" activeclassname="activo" className={Style.navLink} style={location.pathname === "/menu" ? activeStyle : {}} onClick={closeMenu}>Menú</Link>
                 <Link to="/ubicacion" activeclassname="activo" className={Style.navLink} style={location.pathname === "/ubicacion" ? activeStyle : {}} onClick={closeMenu}>Ubicación</Link>
             </div>
